21586c91b8b94396aa1f7df3a131c51ccbe4150c
timiosaze <timiade1993@gmail.com>
2024-10-21 07:32:40 +0100

    Initial commit

---
+++ src/questions.js
@@ -0,0 +1,90 @@
+const questions = [
+    {
+        question:'Who was Nigeria’s first prime minister?',
+        option_a:'Obafemi Awolowo',
+        option_b:'General Aguiyi Ironsi',
+        option_c:'Abubakar Tafawa Balewa',
+        option_d:'Shehu Shagari',
+        answer:'Abubakar Tafawa Balewa'
+    },
+    {
+        question:'Who is the first Nigerian president?',
+        option_a:'Obafemi Awolowo',
+        option_b:'Dr Nnamdi Azikiwe',
+        option_c:'Abubakar Tafawa Balewa',
+        option_d:'Shehu Shagari',
+        answer:'Dr Nnamdi Azikiwe'
+    },
+    {
+        question:'How many ministers do we have in Nigeria?',
+        option_a:'34',
+        option_b:'38',
+        option_c:'43',
+        option_d:'36',
+        answer:'43'
+    },
+    {
+        question:'The first general election in Nigeria was held in',
+        option_a:'1957',
+        option_b:'1960',
+        option_c:'1959',
+        option_d:'1958',
+        answer:'1959'
+    },    
+    {
+        question:'Who was the first Nigerian to become a Nobel Laureate?',
+        option_a:'Obafemi Awolowo',
+        option_b:'Wole Soyinka',
+        option_c:'Ngozi Iweala',
+        option_d:'Chimamanda Ngozi Adichie',
+        answer:'Wole Soyinka'
+    },
+    {
+        question:'Which country won the 2018 World Cup?',
+        option_a:'Brazil',
+        option_b:'France',
+        option_c:'Argentina',
+        option_d:'Italy',
+        answer:'France'
+    },
+    {
+        question:'When did Nigeria become a republic?',
+        option_a:'1959',
+        option_b:'1960',
+        option_c:'1999',
+        option_d:'1963',
+        answer:'1963'
+    },
+    {
+        question:'How many Local Governments do we have in Nigeria?',
+        option_a:'750',
+        option_b:'774',
+        option_c:'776',
+        option_d:'763',
+        answer:'774'
+    },
+    {
+        question:'What is the centenary?',
+        option_a:'150',
+        option_b:'100',
+        option_c:'90',
+        option_d:'99',
+        answer:'100'
+    },   
+    {
+        question:'When was Nigeria formed?',
+        option_a:'1959',
+        option_b:'1915',
+        option_c:'1916',
+        option_d:'1914',
+        answer:'1914'
+    },
+    {
+        question:'Who gave Nigeria her name?',
+        option_a:'Micheal Taiwo Akinkunmi',
+        option_b:'Dr Nnamdi Azikwe',
+        option_c:'Flora Shaw',
+        option_d:'General Aguiyi Ironsi',
+        answer:'Flora Shaw'
+    },
+]
